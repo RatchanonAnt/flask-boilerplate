5502ed3d638e1b2de4000a834a1a91d897cbd21b
Ant <16032045@aru.ac.th>
2020-07-12 13:33:47 +0700

    add app.py

--- app.py
+++ app.py
@@ -7,7 +7,26 @@ CORS(app)
 
 @app.route('/')
 def root():
-    return "Working"
+    return "Yor see my Pikachu?"
+
+@app.route('/new_one')
+def new_one_funtion():
+    username = request.args.get('name')
+    print(username)
+    return "Hi" + username
+
+@app.route('/sum')
+def my_sum():
+   a =request.args.get('a')
+   a =int(a)
+   b =request.args.get('b')
+   b =int(b)
+   return str (a+b)
+
+@app.route('/mypage')
+def mypage():
+    username = request.args.get('name')
+    return render_template('home.html',name = username)
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
